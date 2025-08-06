a49c40fcac8f7543213e8e35904c2924da05d663
stardust54 <a1205993426@163.com>
2025-08-07 00:55:27 +0800

    fix

---
+++ flower/scripts_and_exp_env/run_comc1_trinity.bat
@@ -0,0 +1,14 @@
+@echo off
+set LOGFILE=comc1food_trinity_log.txt
+echo. > %LOGFILE%
+
+REM Baseline 模型
+python Comc1_flower_trinity.py --model_path "models/comc1.pth" --running_mod "eval" --plot_only "False" --erase_ratio 0 >> %LOGFILE% 2>&1
+python Comc1_flower_trinity.py --model_path "models/comc1.pth" --running_mod "eval" --plot_only "False" --erase_ratio 0.1 >> %LOGFILE% 2>&1
+python Comc1_flower_trinity.py --model_path "models/comc1.pth" --running_mod "eval" --plot_only "False" --erase_ratio 0.2 >> %LOGFILE% 2>&1
+python Comc1_flower_trinity.py --model_path "models/comc1.pth" --running_mod "eval" --plot_only "False" --erase_ratio 0.3 >> %LOGFILE% 2>&1
+python Comc1_flower_trinity.py --model_path "models/comc1.pth" --running_mod "eval" --plot_only "False" --erase_ratio 0.4 >> %LOGFILE% 2>&1
+python Comc1_flower_trinity.py --model_path "models/comc1.pth" --running_mod "eval" --plot_only "False" --erase_ratio 0.5 >> %LOGFILE% 2>&1
+
+echo Done! All evaluations have been completed.
+pause
